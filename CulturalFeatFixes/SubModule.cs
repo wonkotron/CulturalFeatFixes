6dd4d8cad79885beb426020dff3562ea87c81385
wonkotron <wonkotron@gmail.com>
2020-04-14 02:18:38 -0700

    Fix conversion error for feat bonus calculation (#1)
    * fix calculation in AddToStat
    
    * use non system specific path for debug build output
    
    * update patch comment
    
    * remove test loading notification
    
    * fix release build by disabling code optimization
    
    * file formatting

---
+++ CulturalFeatFixes/SubModule.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace CulturalFeatFixes {
+    public class SubModule : MBSubModuleBase {
+        public static readonly string ModuleName = "CulturalFeatFixes";
+
+        protected override void OnSubModuleLoad() {
+            base.OnSubModuleLoad();
+
+            var harmony = new Harmony("mod.bannerlord.wonkotron");
+            harmony.PatchAll();
+        }
+    }
+}
