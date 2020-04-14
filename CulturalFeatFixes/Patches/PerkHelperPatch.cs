1dbdb517bf1bfde011bfab30c4fc3f46b00c202b
wonkotron <wonkotron@gmail.com>
2020-04-14 02:12:16 -0700

    update patch comment

--- CulturalFeatFixes/Patches/PerkHelperPatch.cs
+++ CulturalFeatFixes/Patches/PerkHelperPatch.cs
@@ -6,6 +6,9 @@ using Helpers;  // From Taleworlds.CampaignSystem.dll
 namespace CulturalFeatFixes.Patches {
     [HarmonyPatch(typeof(PerkHelper), "AddFeatBonusForPerson")]
     public class PerkHelperPatch {
+        // Fixes issue where effect bonuses are incorrectly converted from percentage to fractional value
+        // Effect bonuses are already stored in fractional values
+        // See TaleWorlds.CampaignSystem.DefaultFeats.InitializeAll() [e1.1.0 beta as of 2020-04-14]
         static bool Prefix(FeatObject feat, CharacterObject character, ref ExplainedNumber bonuses) {
             if (character != null && character.GetFeatValue(feat)) {
                 Patched_AddToStat(ref bonuses, feat.IncrementType, feat.EffectBonus, feat.Name);
@@ -14,29 +17,24 @@ namespace CulturalFeatFixes.Patches {
             return false;  // Skip real AddFeatBonusForPerson()
         }
 
-		/*
-		 * Developer Note:
-		 * 
-		 * TaleWorlds Helpers.PerkHelper.AddToStat functions multiply feat.EffectBonus by 0.01f, but EffectBonus factors are entered in their fractional value (0.01 vs 10%).
-		 */
-		private static void Patched_AddToStat(ref ExplainedNumber stat, SkillEffect.EffectIncrementType effectIncrementType, float number, TextObject text) {
-			if (effectIncrementType == SkillEffect.EffectIncrementType.Add) {
-				stat.Add(number, text);
-				return;
-			}
-			if (effectIncrementType == SkillEffect.EffectIncrementType.AddFactor) {
-				stat.AddFactor(number, text);
-			}
-		}
+        private static void Patched_AddToStat(ref ExplainedNumber stat, SkillEffect.EffectIncrementType effectIncrementType, float number, TextObject text) {
+            if (effectIncrementType == SkillEffect.EffectIncrementType.Add) {
+                stat.Add(number, text);
+                return;
+            }
+            if (effectIncrementType == SkillEffect.EffectIncrementType.AddFactor) {
+                stat.AddFactor(number, text);
+            }
+        }
 
-		private static void Patched_AddToStat(ref ExplainedNumber stat, FeatObject.AdditionType additionType, float number, TextObject text) {
-			if (additionType == FeatObject.AdditionType.Add) {
-				stat.Add(number, text);
-				return;
-			}
-			if (additionType == FeatObject.AdditionType.AddFactor) {
-				stat.AddFactor(number, text);
-			}
-		}
-	}
+        private static void Patched_AddToStat(ref ExplainedNumber stat, FeatObject.AdditionType additionType, float number, TextObject text) {
+            if (additionType == FeatObject.AdditionType.Add) {
+                stat.Add(number, text);
+                return;
+            }
+            if (additionType == FeatObject.AdditionType.AddFactor) {
+                stat.AddFactor(number, text);
+            }
+        }
+    }
 }
