29032ac14138e351ae6c9210b4f41ae0ba6cc60c
wonkotron <wonkotron@gmail.com>
2020-04-14 16:11:15 -0700

    Fix khuzait calculation (#6)
    * add copy of DefaultPartySpeedCalculatingModel
    
    * implement khuzait calculation fix
    
    * add replacement model loading during initialization

--- CulturalFeatFixes/SubModule.cs
+++ CulturalFeatFixes/SubModule.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using CulturalFeatFixes.Models;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
@@ -12,6 +14,18 @@ namespace CulturalFeatFixes {
 
             var harmony = new Harmony("mod.bannerlord.wonkotron");
             harmony.PatchAll();
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject) {
+            base.OnGameStart(game, gameStarterObject);
+
+            AddModels(gameStarterObject as CampaignGameStarter);
+        }
+
+        private void AddModels(CampaignGameStarter gameStarter) {
+            if (gameStarter != null) {
+                gameStarter.AddModel(new CFF_PartySpeedCalculatingModel());
+            }
         }
     }
 }
