dfbb04b9d4bb00a072d9c607228843ab9a95e2b0
wonkotron <wonkotron@gmail.com>
2020-04-15 22:08:57 -0700

    Patched in workaround for Aserai Caravan cost reduction (#10)

--- CulturalFeatFixes/SubModule.cs
+++ CulturalFeatFixes/SubModule.cs
@@ -1,25 +1,34 @@
-﻿using CulturalFeatFixes.Models;
+﻿using System;
+using System.IO;
+using CulturalFeatFixes.Models;
+using CulturalFeatFixes.Patches;
 using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
-using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
 namespace CulturalFeatFixes {
     public class SubModule : MBSubModuleBase {
+        public static readonly Harmony harmony = new Harmony("mod.bannerlord.wonkotron");
         public static readonly string ModuleName = "CulturalFeatFixes";
 
+        // @TODO:  Refactor mod loading method for better maintainability
         protected override void OnSubModuleLoad() {
             base.OnSubModuleLoad();
 
-            var harmony = new Harmony("mod.bannerlord.wonkotron");
-            harmony.PatchAll();
+            PerkHelperPatch.Apply();
         }
 
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject) {
             base.OnGameStart(game, gameStarterObject);
 
             AddModels(gameStarterObject as CampaignGameStarter);
+        }
+
+        public override void OnGameInitializationFinished(Game game) {            
+            LordConversationsCampaignBehavior_Patch.Apply();
+
+            base.OnGameInitializationFinished(game);
         }
 
         private void AddModels(CampaignGameStarter gameStarter) {
